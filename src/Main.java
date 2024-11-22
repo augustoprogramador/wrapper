079feeb6bb592c4ee308f6e7e7e9741f8235e196
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2024-11-22 08:26:46 -0300

    terminado

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double val = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Informe um valor:");
+        val = scan.nextDouble();
+        Double val2 = val;
+        System.out.println((String.format("Esse é o valor de val %.2f", val)));
+        System.out.println((String.format("Esse é o valor de val2 %.2f", val2)));
+        System.out.println("Comparando os valoes de val e val2, usando o método equals do val2");
+        boolean iguais = val2.equals(val);
+        System.out.println((String.format("Eles são: %b", iguais)));
+    }
 }
